3cf8b011ddc19d803999c2cb1b99835c12d5b30c
calvinc03 <43226170+calvinc03@users.noreply.github.com>
2024-12-17 17:51:20 -0800

    Updates

---
+++ precache-manifest.69f4528ed7ddc5118a4152b6ebeeff87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e6975a361871377da488b6b299beacc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8ac0a373673955b8ee0",
+    "url": "/static/css/main.114c6689.chunk.css"
+  },
+  {
+    "revision": "fb9c4e6f8337909a1c0d",
+    "url": "/static/js/2.92926c1f.chunk.js"
+  },
+  {
+    "revision": "dc005ab625f2f3aa19ae2032d9516052",
+    "url": "/static/js/2.92926c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ac0a373673955b8ee0",
+    "url": "/static/js/main.7c2a5bfb.chunk.js"
+  },
+  {
+    "revision": "4a622ea621e71266c231",
+    "url": "/static/js/runtime-main.2b353301.js"
+  },
+  {
+    "revision": "8f721990969646344a72795352214d0d",
+    "url": "/static/media/me.8f721990.jpg"
+  },
+  {
+    "revision": "548296f3c0785b33caadc71d11623d39",
+    "url": "/static/media/rose.548296f3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.27b3553f5a4537265e0fb90d74d26355.js"
+  "/precache-manifest.69f4528ed7ddc5118a4152b6ebeeff87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
